c72f48c6e59ce09d97ded78b75f16765f1da3472
ekriu <dev@datafiche.com>
2023-02-05 18:03:12 +0200

    parse errors

--- src/components/ErrorContextProvider.tsx
+++ src/components/ErrorContextProvider.tsx
@@ -3,12 +3,12 @@ import { createContext } from "react";
 
 type ContextType = {
     error: string,
-    setError: (error: string) => void,
+    setError: (error: any) => void,
 }
 
 const defaultValue = {
     error: '',
-    setError: (_error: string) => {
+    setError: (_error: any) => {
         throw new Error('Context not initialized');
     },
 }
@@ -17,15 +17,20 @@ const ErrorContext = createContext<ContextType>(defaultValue);
 export const ErrorContextProvider = ({ children }: PropsWithChildren) => {
   const [error, setError] = useState(defaultValue.error);
   return (
-    <ErrorContext.Provider value={{ error, setError: (error) => {
-      // TODO parse error
-      const errors = e.errors.reduce((acc: any, error: any) => {
-        return acc += error.message; 
-      }, '');
-
-      if (error) 
+    <ErrorContext.Provider value={{ error, setError: (e) => {
+      if (e) {
         console.error('AppError', error);
-      setError(error);
+        if (e.errors) {
+          const errors = e.errors.reduce((acc: any, error: any) => {
+            return acc += error.message + '; '; 
+          }, '');
+
+          setError(errors);
+          return;
+        }
+      }
+
+      setError(e);
     }}}>
       {children}
     </ErrorContext.Provider>
