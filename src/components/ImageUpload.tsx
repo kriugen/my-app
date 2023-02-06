6c51354b6181c166048d3985f777b8a699174df5
ekriu <dev@datafiche.com>
2023-02-06 16:30:57 +0200

    refactor

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -7,62 +7,52 @@ export default function ImageUpload({ imageUrl, setImage }: any) {
   const maxHeight = 150;
   const maxWidth = 200;
 
-  return <>
-      { 
-        src ? <>
-          <label
-            htmlFor="post-image">
-            <Box sx={{ 
-                width: maxWidth, 
-                height: maxHeight, 
-                border: '1px dashed',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}>
-              <Box
-                component="img"
-                sx={{
-                  maxWidth,
-                  maxHeight
-                }}
-                alt="The house from the offer."
-                src={ src }
-              />
-            </Box>
-          </label>
-          <ImageInput style={{ display: 'none' }} 
-            setImage={setImage}
-            setSrc={setSrc} 
-          />
-        </>
-      : <>
-        <button style={{ 
+  return src 
+    ? <Box sx={{
+        width: maxWidth,
+        height: maxHeight,
+        border: '1px dashed',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Box
+          component="img"
+          sx={{
+            maxWidth,
+            maxHeight
+          }}
+          src={ src }
+        />
+        <ImageInput 
+          setImage={setImage}
+          setSrc={setSrc} 
+        />
+      </Box>
+    : <Box sx={{
           border: '1px dashed',
           height: maxHeight,
           width: maxWidth,
           position: 'relative' as const,
         }}>
-          <ImageInput style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            opacity: 0,
-          }} 
-            setImage={setImage}
-            setSrc={setSrc} 
-          />
-        </button>
-      </> }
-  </>
+        <ImageInput 
+          setImage={setImage}
+          setSrc={setSrc} 
+        />
+      </Box>
 }
 
-function ImageInput({ style, setImage, setSrc }: any) {
+function ImageInput({ setImage, setSrc }: any) {
   return <input
             id='post-image'
-            style={style}
+            style={{ 
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              opacity: 0,
+            }}
             type='file'
             onChange={(e: any) => {
               const fileUploaded = e.target.files[0];
