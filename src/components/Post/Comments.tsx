03fb6fad86f7be26e5ccce7d663009ce1f95b377
ekriu <dev@datafiche.com>
2023-02-22 18:55:59 +0700

    comment design

--- src/components/Post/Comments.tsx
+++ src/components/Post/Comments.tsx
@@ -1,33 +1,24 @@
-import { Avatar, Badge, Box } from "@mui/material";
-import { styled } from '@mui/material/styles'
-
-const BadgeContentSpan = styled('span')(({ theme }) => ({
-  width: 8,
-  height: 8,
-  borderRadius: '50%',
-  backgroundColor: theme.palette.success.main,
-  boxShadow: `0 0 0 2px ${theme.palette.background.paper}`
-}))
+import { Avatar, Box, Card, CardActionArea, CardContent, Typography } from "@mui/material";
 
 const Comments = ({ post }: any) => {
   return post?.comments?.items.map((c: any) => 
-    <Box key={c.id}>
-      <Badge
-        overlap='circular'
-        sx={{ ml: 2, cursor: 'pointer' }}
-        badgeContent={<BadgeContentSpan />}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        data-test='avatar'
-      >
-        <Avatar
-          sx={{ width: 40, height: 40 }}
-          src='/images/avatars/broken-link.png'
-          alt={ c.createdBy }
-        />
-      </Badge>
-      {c.createdBy}&nbsp;{c.createdAt}
-      <Box>{c.message}</Box>
-    </Box>)
+    <Card key={c.id} sx={{ m: 1, width: '100%'}}>
+      <CardActionArea sx={{ display: 'flex', minHeight: 140 }}>
+      <Avatar
+        sx={{ width: 40, height: 40 }}
+        src='/images/avatars/broken-link.png'
+        alt={ c.createdBy }
+      />
+        <CardContent sx={{ width: 1 }}>
+          <Typography sx={{ fontSize: 12 }} color="text.secondary" gutterBottom>
+            by: { c.createdBy }&nbsp;{c.createdAt}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {c.message}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>)
 };
 
 export default Comments;
