82614f9b98e9e69f32a5c422b2cbe141573dce68
ekriu <dev@datafiche.com>
2023-02-23 17:17:18 +0700

    comment edit delete icons

--- src/components/Post/Comment.tsx
+++ src/components/Post/Comment.tsx
@@ -1,17 +1,21 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, IconButton, Typography } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 const Comment = ({ comment }: any) => {
   return <Box sx={{ m: 2, width: '100%' }}>
-    <Box sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
       <Avatar
         sx={{ width: 40, height: 40 }}
         src='/images/avatars/broken-link.png'
         alt={comment.createdBy}
       />
-      <Typography sx={{ fontSize: 12 }} color="text.secondary">
+      <Typography sx={{ fontSize: 12, m: 3 }} color="text.secondary">
         {comment.createdBy}
       </Typography>
       <Typography>{comment.createdAt}</Typography>
+      <IconButton><EditIcon /></IconButton>
+      <IconButton><DeleteIcon /></IconButton>
     </Box>
     <Typography sx={{ pl: 12 }} component="div">
       {comment.message}
