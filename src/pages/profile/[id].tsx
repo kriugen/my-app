f74c772d072040f72ab5d515b499b70c201bf2e7
ekriu <dev@datafiche.com>
2023-03-30 12:22:27 +1100

    profile get / create

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,3 +1,16 @@
+import { API } from "aws-amplify";
+import { createProfile } from "src/graphql/mutations";
+
+const getProfile = `
+  query GetProfile($id: ID!) {
+    getProfile(id: $id) {
+      id
+      firstName
+      lastName
+      DOB
+    }
+  }`;
+
 function Profile({ id }: any) {
   return <div>Profile for user sub {id}</div>
 }
@@ -5,9 +18,40 @@ function Profile({ id }: any) {
 export const getServerSideProps: any = async ({ params }: any) => {
   const { id } = params;
 
+  const profileData: any = await API.graphql({
+    query: getProfile,
+    variables: { id },
+  });
+
+  let data = profileData.data.getPost;
+
+  console.log('PROFILE DATA',)
+  if (!data) {
+    try {
+      data = {
+        id
+      };
+
+      await API.graphql({
+        query: createProfile,
+        variables: {
+          input: data,
+        },
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      });
+    } catch (e: any) {
+      console.log('ERROR', e)
+      return {
+        props: {
+          error: e,
+        }
+      }
+    }
+  }
+
   return {
     props: {
-      id,
+      id: data.id,
     },
   };
 }
